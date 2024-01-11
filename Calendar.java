7a0ed81aa98a460203a34c207911fb31e913946e
YotaMarkman <yotam.markman@post.runi.ac.il>
2024-01-11 16:17:24 +0200

    HW3

--- Calendar.java
+++ Calendar.java
@@ -1,40 +1,31 @@
 public class Calendar
 {
-   static int dayOfMonth = 1;  
+    
+	static int dayOfMonth = 1;  
 	static int month = 1;
 	static int year = 1900;
 	static int dayOfWeek = 2;     
 	static int nDaysInMonth = 31; 
 	public static void main(String args[])
-	{
-		String day = "";
+	{ 
+		String str = "";
 		int pickedY = Integer.parseInt(args[0]);
 		while (year < pickedY + 1)
 		{
 			advance();
 			while(year == pickedY)
 			{
-				day = "";
-				if(dayOfWeek == 1)
-				{					
-					day = " Sunday";
-					System.out.println(dayOfMonth + "/" + month + "/" + year + day);
-					advance();
-				}
+				str = "";
+				if(dayOfWeek == 1) str = " Sunday";
+				System.out.println(dayOfMonth + "/" + month + "/" + year + str);
+				advance();
 			}
-        
-		}
+		}       
 	}
 	private static void advance()
 	{
-		if(dayOfWeek == 7)
-		{			
-			dayOfWeek = 1;
-		}
-		else 
-		{
-			dayOfWeek++;
-		}
+		if(dayOfWeek == 7) dayOfWeek = 1;
+		else dayOfWeek++;
 		if(dayOfMonth == nDaysInMonth)
 		{
 			if(month == 12)
@@ -42,14 +33,11 @@ public class Calendar
 				month = 0;
 				year++;
 			}
-		dayOfMonth = 1;
-		month++;
-		nDaysInMonth = nDaysInMonth(month, year);
+			dayOfMonth = 1;
+			month++;
+			nDaysInMonth = nDaysInMonth(month, year);
 		}
-		else
-		{
-			dayOfMonth++;
-		}
+		else dayOfMonth++;
 	}
 	private static boolean isLeapYear(int year) 
 	{
@@ -57,28 +45,19 @@ public class Calendar
 		{
 			return true;
 		}
-		else
+		else 
 		{
 			return false;
 		}
 	}
-	private static int nDaysInMonth(int month, int year)//Returns number of days in month.
+	private static int nDaysInMonth(int month, int year)
 	{
-		if((month == 1) || (month == 3) || (month == 5) || (month == 7) || (month == 8) || (month == 10) || (month == 12))
-		{	
-			return 31;
-		}
-		else if((month == 4) || (month == 6) || (month == 9) || (month == 11))
+		if (((month == 4) || (month == 6) || (month == 9) || (month == 11)))
 		{
 			return 30;
-		}
-		else if(isLeapYear(year))
-		{
-			return 29;
-		}	
-		else
-		{
-			return 28;
-		}
+		} 	
+		else if ((month == 1) || (month == 3) || (month == 5) || (month == 7) || (month == 8) || (month == 10) || (month == 12)) return 31;
+		else if(isLeapYear(year)) return 29;
+		else return 28;
 	}
 }
